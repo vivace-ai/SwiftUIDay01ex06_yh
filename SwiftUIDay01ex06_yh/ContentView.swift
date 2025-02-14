24c1126b61ccb01505e2fd04b4190ba9c50035ed
영감을 주고 희망을 주는 김영희 강사입니다. <openvision4@gmail.com>
2025-02-14 18:56:09 +0900

    SwiftUI View Preview
    Xcode는 PreviewProvider 프로토콜을 준수하는 구조체를 찾아서, 그 구조체의 previews 속성을 사용하여 Canvas에 뷰를 표시합니다.

--- SwiftUIDay01ex06_yh/ContentView.swift
+++ SwiftUIDay01ex06_yh/ContentView.swift
@@ -7,18 +7,30 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Previews_Intro: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        VStack (spacing: 20){
+            Text("Previews")
+                .font(.largeTitle)
+            
+            Text("Introduction")
+                .foregroundColor(.gray)
+            
+            Text("Xcode는 PreviewProvider 프로토콜을 준수하는 구조체를 찾아서, 그 구조체의 previews 속성을 사용하여 Canvas에 뷰를 표시합니다.")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(Color.orange)
+                .foregroundColor(.white)
+            
+        }.font(.title)
     }
 }
 
-#Preview {
-    ContentView()
+// Xcode는 PreviewProvider 구조체를 찾습니다
+struct Previews_Intro_Previews: PreviewProvider {
+    // Canvas가 표시된 경우 Canvas에 보여줄 뷰를 가져오기 위해 이 속성에 접근합니다
+    static var previews: some View {
+        // 프리뷰를 보려면 이 속성 내에서 뷰를 인스턴스화하고 반환하세요.
+        Previews_Intro()
+    }
 }
--- SwiftUIDay01ex06_yh/SwiftUIDay01ex06_yhApp.swift
+++ SwiftUIDay01ex06_yh/SwiftUIDay01ex06_yhApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUIDay01ex06_yhApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Previews_Intro()  // ContentView() 대신 Previews_Intro() 사용
         }
     }
 }
